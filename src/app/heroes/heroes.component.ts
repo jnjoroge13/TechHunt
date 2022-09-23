fbee16d510c67cf678f7759ffa4a5c1430612b78
Jesse <jnjoroge13@gmail.com>
2022-09-23 16:27:53 -0700

    first half part 4

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,27 +1,29 @@
-import { Component, OnInit} from '@angular/core';
-import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { Component, OnInit } from "@angular/core";
+import { Hero } from "../hero";
+import { HEROES } from "../mock-heroes";
+import { HeroService } from "../hero.service";
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+	selector: "app-heroes",
+	templateUrl: "./heroes.component.html",
+	styleUrls: ["./heroes.component.css"],
 })
 export class HeroesComponent implements OnInit {
+	selectedHero?: Hero;
+	onSelect(hero: Hero): void {
+		if (this.selectedHero == hero) {
+			this.selectedHero = undefined;
+		} else {
+			this.selectedHero = hero;
+		}
+	}
 
-  selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    if (this.selectedHero == hero) {
-      this.selectedHero = undefined
-    } else {
-      this.selectedHero = hero
-    }
-  }
+	heroes: Hero[] = [];
 
-  heroes = HEROES
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	constructor(private heroService: HeroService) {}
+	getHeroes() {
+		this.heroes = this.heroService.getHeroes();
+	}
+	ngOnInit(): void {
+		this.getHeroes();
+	}
 }
