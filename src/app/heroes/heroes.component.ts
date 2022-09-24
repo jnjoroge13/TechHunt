958673ed380b4883eb6d3565cc94440ed6d6b8f6
Jesse <jnjoroge13@gmail.com>
2022-09-23 22:20:13 -0700

    4. Add services complete

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { Hero } from "../hero";
 import { HEROES } from "../mock-heroes";
 import { HeroService } from "../hero.service";
+import { MessageService } from "../message.service";
 @Component({
 	selector: "app-heroes",
 	templateUrl: "./heroes.component.html",
@@ -9,17 +10,19 @@ import { HeroService } from "../hero.service";
 })
 export class HeroesComponent implements OnInit {
 	selectedHero?: Hero;
-	onSelect(hero: Hero): void {
-		if (this.selectedHero == hero) {
+  onSelect(hero: Hero): void {
+    if (this.selectedHero == hero) {
+      this.messageService.add(`${this.selectedHero.name} unselected`)
 			this.selectedHero = undefined;
-		} else {
-			this.selectedHero = hero;
+    } else {
+      this.selectedHero = hero;
+      this.messageService.add(`${this.selectedHero.name} selected`)
 		}
 	}
 
 	heroes: Hero[] = [];
 
-	constructor(private heroService: HeroService) {}
+	constructor(private heroService: HeroService, private messageService:MessageService) {}
 	getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes)
